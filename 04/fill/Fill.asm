4c7efbbcec76fb95b936913d1cf7abf041540a9a
yihsien0924 <a0965740223@gmail.com>
2022-12-14 08:25:11 +0800

    finish

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,63 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+//p[i]=screen
+//while (p[i] < 24576(KBD)){
+//M[p[i]] = -1
+//p=p+1}
+//參考資料:https://www.youtube.com/watch?v=4_bmvT-h8Zs&feature=youtu.be
+//參考資料:https://www.cnblogs.com/YjmStr/p/15371886.html
+
+(LOOP)
+@KBD
+D=M 
+@BLACK 
+D;JNE //不相等時跳躍
+@WHITE 
+D;JEQ //相等時跳躍
+@LOOP 
+0;JMP //無條件跳出
+(BLACK)
+@SCREEN 
+D=A
+@P 
+M=D //P=SCREEN 
+(BLACKLOOP)
+@P 
+D=M 
+@KBD 
+D=D-A //D=P-KBD 
+@LOOP
+D;JEQ //相等時跳躍
+D=-1 //黑色
+@P 
+A=M     
+M=D
+D=1 
+@P
+D=D+M 
+M=D   
+@BLACKLOOP 
+0;JMP 
+(WHITE) 
+@SCREEN 
+D=A 
+@P //P=SCREEN 
+M=D 
+(WHITELOOP)
+@P 
+D=M 
+@KBD 
+D=D-A //P-KBD 
+@LOOP 
+D;JEQ 
+D=0 
+@P 
+A=M 
+M=D 
+D=1  
+@P
+D=D+M 
+M=D 
+@WHITELOOP
+0;JMP  
--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,34 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+//R2=R1*R0
+
+//R2=0
+//while(R1>R0){
+//    R2=R2+0
+//    R1=R1-1
+//}
+@R2	
+M=0	
+@0
+D=M //R2 = 0
+@END
+D;JEQ //無條件跳出
+
+@R1
+D=M  
+@END
+D;JEQ
+(LOOP)
+@R0	
+D=M	
+@R2	
+M=D+M //R2+R0
+@R1	
+M=M-1
+D=M	//R1=R1-1
+@LOOP
+D;JGT
+(END)
+@END
+0;JMP
